aa191130cdf83704c7d2502f70575eb4e116d583
mponk <42237347+mponk@users.noreply.github.com>
2025-10-29 08:46:42 +0700

    Refactor main.tsx to simplify component structure
    Removed QueryClientProvider and adjusted component structure.

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,17 @@
+// player360-frontend/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
-
-const queryClient = new QueryClient();
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
 );
